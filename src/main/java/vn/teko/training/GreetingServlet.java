9bbdebdb479727fd571c5452ca828f7749bd571e
Dũng Đặng Minh <dung.dm@teko.vn>
2017-07-25 04:37:31 +0700

    Add `logback` as logging framework

--- src/main/java/vn/teko/training/GreetingServlet.java
+++ src/main/java/vn/teko/training/GreetingServlet.java
@@ -1,5 +1,8 @@
 package vn.teko.training;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class GreetingServlet extends HttpServlet {
+    static final Logger LOG = LoggerFactory.getLogger(GreetingServlet.class);
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().println("Hello World!");
@@ -14,11 +19,11 @@ public class GreetingServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        System.out.println("Servlet " + this.getServletName() + " has started");
+        LOG.info("Servlet " + this.getServletName() + " has started");
     }
 
     @Override
     public void destroy() {
-        System.out.println("Servlet " + this.getServletName() + " has stopped");
+        LOG.info("Servlet " + this.getServletName() + " has stopped");
     }
 }
