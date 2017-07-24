55cd62d530025aa171939a08c08ce2a0182a9353
Dũng Đặng Minh <dung.dm@teko.vn>
2017-07-24 19:02:05 +0700

    First code

---
+++ src/main/java/vn/teko/training/GreetingServlet.java
@@ -0,0 +1,24 @@
+package vn.teko.training;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GreetingServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("Hello World!");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet " + this.getServletName() + " has started");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet " + this.getServletName() + " has stopped");
+    }
+}
---
+++ src/test/java/vn/teko/training/GreetingServletTest.java
@@ -0,0 +1,4 @@
+package vn.teko.training;
+
+class GreetingServletTest {
+}
