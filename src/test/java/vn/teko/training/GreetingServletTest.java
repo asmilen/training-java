12fb28506ae627b1084b93642db21a109f9d89b4
Dũng Đặng Minh <dung.dm@teko.vn>
2017-07-25 04:08:46 +0700

    Unit test

--- src/test/java/vn/teko/training/GreetingServletTest.java
+++ src/test/java/vn/teko/training/GreetingServletTest.java
@@ -1,4 +1,24 @@
 package vn.teko.training;
 
-class GreetingServletTest {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GreetingServletTest {
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("Setup GreetingServlet test");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("Teardown GreetingServlet test");
+    }
+
+    @Test
+    public void doGet() throws Exception {
+        assertEquals(1 + 1, 2);
+    }
 }
